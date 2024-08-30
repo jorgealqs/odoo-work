288f02175b2df924d42330672e368bb5a73eb61b
judaqui123 <judaqui123@gmail.com>
2024-08-30 09:15:20 -0500

    [ADD] WORK/Firts_commit

---
+++ gambling/addons/follow_up_football/__manifest__.py
@@ -0,0 +1,21 @@
+{
+    "name": "Follow-up of the leagues ",
+    "version": "1.0",
+    "author": "Jorge Alberto Quiroz Sierra",
+    "category": "FollowUp/Leagues",
+    "description": """
+        This module allows users to manage and follow
+        up on various football leagues. It includes features
+        such as tracking league information, managing teams and
+        players, and updating standings and statistics. Ideal for
+        sports enthusiasts and organizations needing a centralized
+        platform for league management.
+    """,
+    "depends": ["base", "web"],
+    "data": [
+        # "views/my_view.xml",
+    ],
+    "installable": True,
+    "application": True,
+    "auto_install": False,
+}
