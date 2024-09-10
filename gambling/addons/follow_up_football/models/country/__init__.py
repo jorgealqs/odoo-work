b1b5cb6f2f28cda6bb41c59a56aabf3f90ec59ac
judaqui123 <judaqui123@gmail.com>
2024-09-10 10:20:14 -0500

    [DEL] Work/Api_Football_Delete_Unnecessary_Files

--- gambling/addons/follow_up_football/models/country/__init__.py
+++ gambling/addons/follow_up_football/models/country/__init__.py
@@ -1,4 +1,3 @@
 # -*- coding: utf-8 -*-
 
 from . import football_country  # noqa: F401
-from . import football_country_wizard  # noqa: F401
--- gambling/addons/follow_up_football/models/standing/__init__.py
+++ gambling/addons/follow_up_football/models/standing/__init__.py
@@ -4,4 +4,3 @@ from . import standing  # noqa: F401
 from . import stats  # noqa: F401
 from . import home_stats  # noqa: F401
 from . import away_stats  # noqa: F401
-from . import standing_wizard  # noqa: F401
