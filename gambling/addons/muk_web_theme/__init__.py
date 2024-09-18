f469593d43ae2edaefc9c4dd927e0be0e991323c
judaqui123 <judaqui123@gmail.com>
2024-09-17 19:14:50 -0500

    [FIX] Work/Lottery_Baloto_Fix_Search

--- gambling/addons/muk_web_theme/__init__.py
+++ gambling/addons/muk_web_theme/__init__.py
@@ -6,16 +6,15 @@ from odoo.tools import file_open
 
 
 def _setup_module(env):
-    if env.ref('base.main_company', False): 
+    if env.ref('base.main_company', False):
         with file_open('web/static/img/favicon.ico', 'rb') as file:
             env.ref('base.main_company').write({
                 'favicon': base64.b64encode(file.read())
             })
-        with file_open('muk_web_theme/static/src/img/background.png', 'rb') as file:
+        with file_open('muk_web_theme/static/src/img/leon.avif', 'rb') as file:
             env.ref('base.main_company').write({
                 'background_image': base64.b64encode(file.read())
             })
-
 
 def _uninstall_cleanup(env):
     env['res.config.settings']._reset_theme_color_assets()
