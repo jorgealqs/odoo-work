0a89dbe47ba16417aa4ec52f5221b9952aa50b61
judaqui123 <judaqui123@gmail.com>
2024-09-19 11:22:42 -0500

    Fix

--- gambling/addons/lottery_forecast/models/baloto/lottery_baloto_number_frequency_pair.py
+++ gambling/addons/lottery_forecast/models/baloto/lottery_baloto_number_frequency_pair.py
@@ -5,13 +5,6 @@ from odoo import models, fields
 _logger = logging.getLogger(__name__)
 
 
-class LotteryBalotoDraw(models.Model):
-    _name = 'lottery.baloto.draw'
-    _description = 'Lottery Draw Dates'
-
-    draw_date = fields.Date(string="Draw Date", required=True)
-
-
 class LotteryBalotoNumberFrequencyPair(models.Model):
     _name = 'lottery.baloto.number.frequency.pair'
     _description = 'Frequency of Pairs of Numbers in Lottery Draws'
